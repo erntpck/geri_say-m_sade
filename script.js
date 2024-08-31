96f3c0b721d2cdf5c739feefe71f1ea84e559ebf
Eren Tepecik <erntpck@gmail.com>
2024-08-31 09:07:22 +0300

    proje oluşturuldu

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById("aydinButton").addEventListener("click", function() {
+    const targetDate = new Date("2024-09-27T17:00:00");
+    startCountdown(targetDate);
+});
+
+document.getElementById("kalpButton").addEventListener("click", function() {
+    const targetDate = new Date("2024-09-28T11:00:00");
+    startCountdown(targetDate);
+});
+
+document.getElementById("heartContainer").addEventListener("click", function() {
+    window.open("https://youtu.be/BYBGnwwfh9A?si=jQ4JPBIY0auseoN3", "_blank");
+});
+
+function startCountdown(targetDate) {
+    clearInterval(window.countdownInterval); // Önceki geri sayımı temizle
+    window.countdownInterval = setInterval(function() {
+        updateTimeUntil(targetDate);
+    }, 1000);
+}
+
+function updateTimeUntil(targetDate) {
+    const now = new Date();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+        document.getElementById("result").innerText = "Belirtilen tarih geçti.";
+        clearInterval(window.countdownInterval); // Geri sayımı durdur
+        return;
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById("result").innerText = 
+        `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye kaldı.`;
+}
